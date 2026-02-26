ea1e26a90129333ab2b77fe5296d278634910480
HectorFranco-MISO <h.franco@uniandes.edu.co>
2026-02-25 23:51:44 -0500

    Ajuste variables

--- IOTMonitoringServer/settings.py
+++ IOTMonitoringServer/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-bu+)8ft@9+qd*#e#f_s@wkyv2tmq+#!a^3j15h3kjk^jzksu0j
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost", "192.168.0.5"]
+ALLOWED_HOSTS = ["localhost", "13.219.77.134"]
 
 
 # Application definition
